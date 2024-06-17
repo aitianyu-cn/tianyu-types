661683b46f94ea35707097817bb51d31e72d8906
aitianyu macbook <aitianyu.cn@outlook.com>
2024-06-17 08:07:40 +0800

    struct base type extension
    branch: dev
    user: senyun.yao

---
+++ src/utilities/core/interface/ITianyuType.ts
@@ -0,0 +1,7 @@
+/** @format */
+
+import { IComparable } from "src/types";
+
+export interface ITianyuType extends IComparable {
+    toString(): string;
+}
---
+++ src/utilities/core/type/TArray.ts
@@ -0,0 +1,3 @@
+/** @format */
+
+export class TArray<T> {}
